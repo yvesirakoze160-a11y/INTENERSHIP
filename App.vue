8c7a77fb515ac07c4a066f632fa09ddf9e4a9d3e
yvesirakoze160-a11y <yvesirakoze160@gmail.com>
2026-05-11 10:53:47 +0200

    Add files via upload
    this is full package for jumping game.

---
+++ App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <JumpGame />
+  </div>
+</template>
+
+<script>
+import JumpGame from './components/JumpGame.vue'
+
+export default {
+  components: {
+    JumpGame
+  }
+}
+</script>
